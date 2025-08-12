898a7d67ccb180a091b4e89355f02bddc64b9520
Muhammad Anas <anasahmedofficial786@gmail.com>
2025-08-12 09:32:12 +0500

    First commit
    A fast-paced 2D Multiplayer Battle Arena game made with Python and Pygame — choose your fighter, battle against AI or friends, and enjoy smooth animations with dynamic backgrounds.

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ def draw_bg():
   scaled_bg = pygame.transform.scale(bg_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
   screen.blit(scaled_bg, (0, 0))
 
-#function for drawing fighter health bars
+#function for drawing fighter health bars.
 def draw_health_bar(health, x, y):
   ratio = health / 100
   pygame.draw.rect(screen, WHITE, (x - 2, y - 2, 404, 34))
